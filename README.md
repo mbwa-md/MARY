337ea5009c2b8457769586c874768d7f081e6b01
mbwa-md <fundimbwa@gmail.com>
2025-12-30 13:23:32 +0300

    sila md

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# MMMMMMMMMMMH
+##
--- index.js
+++ index.js
@@ -2,28 +2,33 @@ const express = require('express');
 const app = express();
 __path = process.cwd()
 const bodyParser = require("body-parser");
+const path = require('path');
 const PORT = process.env.PORT || 8000;
-let code = require('./pair'); 
+const { router: code, startAllSessions } = require('./main.js');
 
 require('events').EventEmitter.defaultMaxListeners = 500;
 
-app.use('/code', code);
-app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/pair.html')
+app.use(express.static(__path));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__path, 'pair-code.html'));
 });
-app.use('/', async (req, res, next) => {
-    res.sendFile(__path + '/main.html')
+
+app.use('/code', code);
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        status: false,
+        message: '❌ Unknown Endpoint!'
+    });
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(PORT, () => {
-    console.log(`
-Don't Forget To Give Star
-
-
-Server running on http://localhost:` + PORT)
+app.listen(PORT, async () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+    await startAllSessions();
 });
 
 module.exports = app;
